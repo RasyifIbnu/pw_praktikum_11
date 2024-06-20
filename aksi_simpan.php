b68f9ba01c526bdf6af2ae6c2edd0671d64c3606
RasyifIbnu <rasyifibnu23@gmail.com>
2024-06-20 17:13:43 +0700

    new file:   aksi_edit.php

---
+++ aksi_simpan.php
@@ -0,0 +1,20 @@
+<?php
+include "koneksi.php";
+// Mendapatkan data dari form
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$hp = $_POST['no_hp'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$alamat = $_POST['alamat'];
+$jurusan = $_POST['jurusan'];
+
+$sql = "INSERT INTO mahasiswa (nim, nama, alamat, no_hp, jenis_kelamin, jurusan) VALUES ('$nim','$nama', '$alamat', '$hp', '$jenis_kelamin', '$jurusan')";
+
+if (mysqli_query($link, $sql)) {
+    header("location:tampil_data.php");
+} else {
+    header("location:form_tambah.php");
+}
+
+
+?>
